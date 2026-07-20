4d4c6d1219c6e6f593ead267caa60c743ef4150c
Brian Cross <brian.cross.1977@gmail.com>
2026-07-20 08:28:11 -0600

    Add opening hours to business info.

--- src/data/business.ts
+++ src/data/business.ts
@@ -3,6 +3,11 @@ export interface Business {
   legalName: string;
   email: string;
   phone: string;
+  hours: {
+    weekdays: string;
+    weekends: string;
+    holidays: string;
+  };
 }
 
 export const business: Business = {
@@ -10,4 +15,9 @@ export const business: Business = {
   legalName: "Wi-Fi Pros Network Consulting Ltd.",
   email: "info@wifipros.ca",
   phone: "+1 (587) 410-3819",
+  hours: {
+    weekdays: "9:00 AM – 5:00 PM",
+    weekends: "Closed",
+    holidays: "Closed",
+  },
 };
