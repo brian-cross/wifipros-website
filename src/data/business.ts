fb15c27083adbf333b2323e17c86a67c7e23d3ef
Brian Cross <brian.cross.1977@gmail.com>
2026-07-20 08:22:23 -0600

    Add business info to central location.

---
+++ src/data/business.ts
@@ -0,0 +1,13 @@
+export interface Business {
+  name: string;
+  legalName: string;
+  email: string;
+  phone: string;
+}
+
+export const business: Business = {
+  name: "Wi-Fi Pros Network Consulting",
+  legalName: "Wi-Fi Pros Network Consulting Ltd.",
+  email: "info@wifipros.ca",
+  phone: "+1 (587) 410-3819",
+};
